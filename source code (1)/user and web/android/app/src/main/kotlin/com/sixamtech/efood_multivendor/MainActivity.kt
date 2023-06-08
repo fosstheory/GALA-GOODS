2d66d68b115e5baefaddef82dc56e166dc981407
GALA Exchange <kfont@galaexchange.com>
2023-06-08 13:46:54 +0200

    Agregar archivos de proyecto.

---
+++ source code (1)/user and web/android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.galagoods.user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ source code (1)/user and web/web/firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAerSq3MtEcaZqpM7iRy4FHWN084oELSEs",
+  authDomain: "galagoods.firebaseapp.com",
+  projectId: "galagoods",
+  storageBucket: "galagoods.appspot.com",
+  messagingSenderId: "909135229275",
+  appId: "1:909135229275:web:cfea21c5139d3967f66a7e",
+  databaseURL: "...",
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    const promiseChain = clients
+        .matchAll({
+            type: "window",
+            includeUncontrolled: true
+        })
+        .then(windowClients => {
+            for (let i = 0; i < windowClients.length; i++) {
+                const windowClient = windowClients[i];
+                windowClient.postMessage(payload);
+            }
+        })
+        .then(() => {
+            const title = payload.notification.title;
+            const options = {
+                body: payload.notification.score
+              };
+            return registration.showNotification(title, options);
+        });
+    return promiseChain;
+});
+self.addEventListener('notificationclick', function (event) {
+    console.log('notification received: ', event)
+});
